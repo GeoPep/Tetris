a23cd10df00ef1af6a5d18b191cec8e7a32fe3a2
GeoPep <71712532+GeoPep@users.noreply.github.com>
2020-10-13 02:10:06 +0300

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -6,7 +6,7 @@ The game uses data from data folder. Boxes were created using Windows Paint
 Also, the game uses config.dat file in order to store the game options and high score
 The game is available in two screen resolutions. 525x675 and 600x900
 I used simpleaudio for playing sound because i had some compile errors with pygame
-Game was finished on 13 October 2020 by GeoPe and uploaded on GitHub https://github.com/GeoPep
+Game was finished on 13 October 2020 by GeoPep and uploaded on GitHub https://github.com/GeoPep
 """
 import pygame
 import random
